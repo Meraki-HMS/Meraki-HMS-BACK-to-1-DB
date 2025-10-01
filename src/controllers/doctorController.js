45459c7861c538f7310fbd4c4b87f847ba2c16be
Rihan Shaikh <rihanshaikh5598@gmail.com>
2025-10-01 19:51:02 +0530

    "This commit will add Bed assignment features"

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -1,11 +1,18 @@
 const Doctor = require("../models/Doctor");
 const bcrypt = require("bcrypt");
+const Hospital = require("../models/Hospital");
 const generateToken = require("../utils/generateToken");
 
 // ✅ Register Doctor
 exports.registerDoctor = async (req, res) => {
   try {
     const { name, email, contact, password, hospital_id, specialization, workingHours, slotSize, breaks } = req.body;
+
+     // ✅ 1. Check if hospital exists by hospital_id
+    const hospital = await Hospital.findOne({ hospital_id });
+    if (!hospital) {
+      return res.status(400).json({ message: "Invalid hospital_id, hospital not found" });
+    }
 
     // Check if doctor exists
     const existingDoctor = await Doctor.findOne({ email });
@@ -39,11 +46,15 @@ exports.registerDoctor = async (req, res) => {
 // ✅ Login Doctor
 exports.loginDoctor = async (req, res) => {
   try {
-    const { email, password } = req.body;
+    const { email, password  , hospital_id} = req.body;
 
     const doctor = await Doctor.findOne({ email });
     if (!doctor) {
       return res.status(404).json({ message: "Doctor not found" });
+    }
+    const hospital = await Hospital.findOne({ hospital_id });
+    if (!hospital) {
+      return res.status(400).json({ message: "Invalid hospital_id, hospital not found" });
     }
 
     // Compare password
--- src/controllers/receptionistController.js
+++ src/controllers/receptionistController.js
@@ -1,5 +1,6 @@
 const Receptionist = require("../models/Receptionist");
 const generateToken = require("../utils/generateToken");
+const Hospital = require("../models/Hospital");
 
 // 📌 Register Receptionist
 exports.registerReceptionist = async (req, res) => {
@@ -10,6 +11,11 @@ exports.registerReceptionist = async (req, res) => {
     const existing = await Receptionist.findOne({ email });
     if (existing) return res.status(400).json({ message: "Receptionist already exists" });
 
+    const hospital = await Hospital.findOne({ hospital_id });
+    if (!hospital) {
+      return res.status(400).json({ message: "Invalid hospital_id, hospital not found" });
+    }
+    
     const receptionist = await Receptionist.create({
       name, email, mobile, password, hospital_id
     });
@@ -32,10 +38,15 @@ exports.registerReceptionist = async (req, res) => {
 // 📌 Login Receptionist
 exports.loginReceptionist = async (req, res) => {
   try {
-    const { email, password } = req.body;
+    const { email, password , hospital_id } = req.body;
 
     const receptionist = await Receptionist.findOne({ email });
     if (!receptionist) return res.status(400).json({ message: "Receptionist not found" });
+
+    const hospital = await Hospital.findOne({ hospital_id });
+    if (!hospital) {
+      return res.status(400).json({ message: "Invalid hospital_id, hospital not found" });
+    }
 
     const isMatch = await receptionist.matchPassword(password);
     if (!isMatch) return res.status(401).json({ message: "Invalid credentials" });
